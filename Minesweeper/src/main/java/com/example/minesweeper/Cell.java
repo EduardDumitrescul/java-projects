6327ee226ca4125069295f7d3c32b809e2538562
Eduard Dumitrescul <edi.dumitrescul@gmail.com>
2021-11-09 20:41:54 +0200

    added Cell states

--- Minesweeper/src/main/java/com/example/minesweeper/Cell.java
+++ Minesweeper/src/main/java/com/example/minesweeper/Cell.java
@@ -3,4 +3,15 @@ package com.example.minesweeper;
 import javafx.scene.layout.Pane;
 
 public class Cell extends Pane {
+    public static final int EMPTY = 0, EXPOSED_NUMBER = 1, EXPOSED_BOMB = 2;
+
+    private int state = 0;
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public int getState() {
+        return state;
+    }
 }
